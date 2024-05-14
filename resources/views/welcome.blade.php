ca8e45bce7f51b3220744715b191d355b6e8ff19
Abdurahman Zuhair <abdurrahman.zuh@gmail.com>
2024-05-14 14:47:30 +0700

    p

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,11 @@
 @section('title', 'Home')
 
 @section('content')
-    <div class="container">
+    <div class="container py-3">
         <div class="row justify-content-center">
             <div class="col-md-8 text-center">
                 <h1>Book Self</h1>
-                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt
-                    ut labore et dolore magna aliqua.</p>
+                <p class="lead">Mau buat buku????. Coba aja klik tombol di bawah</p>
                 <a href="{{ route('category.index') }}" class="btn btn-primary btn-lg">Explore Categories</a>
             </div>
         </div>
